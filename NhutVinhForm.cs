44305e5636693655898761d8031ab33daebe8e04
Kiyo <lequynhnhutvinh@gmail.com>
2024-01-26 17:38:34 +0700

    chinh sua

--- NhutVinhForm.cs
+++ NhutVinhForm.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void NhutVinhForm_Load(object sender, EventArgs e)
         {
-            // NHut Vinh tao form
+            // NHut Vinh 1 tao form
         }
     }
 }
